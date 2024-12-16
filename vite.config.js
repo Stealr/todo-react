7999e6ddf1890265b310c2ca350304f8db955947
Stealr <baranovm003@yandex.ru>
2024-12-16 14:11:09 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/todo-react/', // Замените <repository> на имя вашего репозитория
   plugins: [react()],
 })
